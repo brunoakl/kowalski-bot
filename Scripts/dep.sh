30cb2e1d86c0036412edf62bf00545e9bc9aedbc
Bruno Machado Ferreira <64101776+brunoakl@users.noreply.github.com>
2023-08-24 10:16:23 -0400

    Instala depencências

---
+++ Scripts/dep.sh
@@ -0,0 +1,8 @@
+echo " "
+echo "          Instalando dependencias"
+pip install keras, tensorflow
+#Inserir aqui os comandos para instalar as dependencias
+#Recomendado rodar o reinstall.sh para toda que for adicionada
+
+#No dia que esses scripts foram criados(10/08/2023), temos a opcao de
+#usar o Python 3.10 nativo do Conda ou 3.11 do script
