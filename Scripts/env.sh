9b16b5d21361b3aab2b23340a72567c4fd21e4de
Bruno Machado Ferreira <64101776+brunoakl@users.noreply.github.com>
2023-08-24 10:28:17 -0400

    Instalar pacotes e o Python 3.11

---
+++ Scripts/env.sh
@@ -0,0 +1,9 @@
+echo " "
+echo "          Instalando pacotes"
+sudo apt install wget build-essential libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+echo " "
+
+echo "          Instalando o Python 3.11"
+sudo apt install python3.11
+echo " "
+
